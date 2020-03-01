f884c5419d971bbf4a37da2c64164504530b468a
JWK1990 <jwkumm@gmail.com>
2020-03-01 13:41:24 +0100

    Working on DND. Positions now added and working,

--- frontend/src/components/draftRoom/teams/MyTeam.jsx
+++ frontend/src/components/draftRoom/teams/MyTeam.jsx
@@ -2,12 +2,57 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
+const roster = {id: 2, type: "22222", defenders: 2, midfielders: 3, rucks: 1, forwards: 2, bench: 4};
+const playerList = {
+    def: [{name: "Josh Gibson", position: "DEF"}],
+    mid: [{name: "Sam Mitchell", position: "MID"}],
+    ruc: [{name: "Max Bailey", position: "RUC"}],
+    fwd: [{name: "Cyril Rioli", position: "FWD"}],
+    bench: [{name: "Jarryd Roughead", position: "BEN"}],
+};
+
 // fake data generator
-const getItems = (count, offset = 0) =>
+const getItems = (count, offset = 0, position) =>
     Array.from({ length: count }, (v, k) => k).map(k => ({
         id: `item-${k + offset}`,
-        content: `item ${k + offset}`
-    }));
+        content: {name: `test-${k + offset}`, position: `${position}`}
+    })
+);
+
+const getPositionList = (position, offset, slots, currentPlayers) => {
+    const playerList = [];
+    for(let i=0; i < slots; i++) {
+        if(currentPlayers[i]) {
+            playerList.push({id: `item-${offset + i}`, content: currentPlayers[i]});
+        } else {
+            playerList.push({id: `item-${offset + i}`, content: {name: "TBA", position: position}})
+        }
+    }
+    return playerList;
+};
+
+const getInitialPositionState = () => {
+        let offset = 0;
+        let initialState = {
+            defs: '',
+            mids: '',
+            rucs: '',
+            fwds: '',
+            bench: '',
+        }
+
+        initialState.defs = getPositionList('DEF', offset, roster.defenders, playerList.def);
+        offset += roster.defenders;
+        initialState.mids = getPositionList('MID', offset, roster.midfielders, playerList.mid);
+        offset += roster.midfielder;
+        initialState.rucs = getPositionList('RUC', offset, roster.rucks, playerList.ruc);
+        offset += roster.rucks;
+        initialState.fwds = getPositionList('FWD', 15, roster.forwards, playerList.fwd);
+        offset += roster.forwards;
+        initialState.bench = getPositionList('BEN', 20, roster.bench, playerList.bench);
+
+        return initialState;
+};
 
 // a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
@@ -24,14 +69,17 @@ const reorder = (list, startIndex, endIndex) => {
 const move = (source, destination, droppableSource, droppableDestination) => {
     const sourceClone = Array.from(source);
     const destClone = Array.from(destination);
+    console.log('Source: ', sourceClone)
+    console.log('Dest: ', destClone)
     const [removed] = sourceClone.splice(droppableSource.index, 1);
+        console.log('Removed: ', removed)
 
     destClone.splice(droppableDestination.index, 0, removed);
 
     const result = {};
     result[droppableSource.droppableId] = sourceClone;
     result[droppableDestination.droppableId] = destClone;
-
+    console.log("Result: ", result);
     return result;
 };
 
@@ -57,31 +105,34 @@ const getListStyle = isDraggingOver => ({
 });
 
 class MyTeam extends Component {
-    state = {
-        items: [{id: 'item-0', content: 'DEF'}, {id: 'item-1', content: 'DEF'}],
-        selected: [{id: 'item-2', content: 'FWD'}, {id: 'item-3', content: 'FWD'}]
-    };
+
+    state = getInitialPositionState();
+
 
     /**
      * A semi-generic way to handle multiple lists. Matches
      * the IDs of the droppable container to the names of the
      * source arrays stored in the state.
      */
-    id2List = {
-        droppable: 'items',
-        droppable2: 'selected'
+    droppableList = {
+        droppableDefs: 'defs',
+        droppableMids: 'mids',
+        droppableRucs: 'rucs',
+        droppableFwds: 'fwds',
+        droppableBench: 'bench',
     };
 
-    getList = id => this.state[this.id2List[id]];
+    getList = id => this.state[this.droppableList[id]];
 
     onDragStart = start => {
-        this.setState({currentPosition: "DEF"});
-        console.log(this.state.currentPosition);
+        const list = this.getList(start.source.droppableId)
+        const index = start.source.index;
+        this.setState({draggedPlayerPosition: list[index].content.position})
+        console.log(this.state.draggedPlayerPosition);
     }
 
     onDragUpdate = update => {
-        this.setState({currentPosition: "DEF"});
-        console.log(this.state.currentPosition);
+        console.log("Update");
     }
 
     onDragEnd = result => {
@@ -93,19 +144,12 @@ class MyTeam extends Component {
         }
 
         if (source.droppableId === destination.droppableId) {
-            const items = reorder(
+            const result = reorder(
                 this.getList(source.droppableId),
                 source.index,
                 destination.index
             );
-
-            let state = { items };
-
-            if (source.droppableId === 'droppable2') {
-                state = { selected: items };
-            }
-
-            this.setState(state);
+            this.setState({[this.droppableList[source.droppableId]]: result})
         } else {
             const result = move(
                 this.getList(source.droppableId),
@@ -113,25 +157,23 @@ class MyTeam extends Component {
                 source,
                 destination
             );
-
-            this.setState({
-                items: result.droppable,
-                selected: result.droppable2
-            });
+            this.setState({[this.droppableList[source.droppableId]]: result[source.droppableId]})
+            this.setState({[this.droppableList[destination.droppableId]]: result[destination.droppableId]})
         }
     };
 
     // Normally you would want to split things out into separate components.
     // But in this example everything is just done in one place for simplicity
     render() {
+        console.log(this.state)
         return (
             <DragDropContext onDragStart={this.onDragStart} onDragUpdate={this.onDragUpdate} onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable" isDropDisabled={this.state.currentPosition != 'FWD'}>
+                <Droppable droppableId="droppableDefs" isDropDisabled={this.state.draggedPlayerPosition != 'DEF'}>
                     {(provided, snapshot) => (
                         <div
                             ref={provided.innerRef}
                             style={getListStyle(snapshot.isDraggingOver)}>
-                            {this.state.items.map((item, index) => (
+                            {this.state.defs.map((item, index) => (
                                 <Draggable
                                     key={item.id}
                                     draggableId={item.id}
@@ -145,7 +187,7 @@ class MyTeam extends Component {
                                                 snapshot.isDragging,
                                                 provided.draggableProps.style
                                             )}>
-                                            {item.content}
+                                            {item.content.name}
                                         </div>
                                     )}
                                 </Draggable>
@@ -154,12 +196,12 @@ class MyTeam extends Component {
                         </div>
                     )}
                 </Droppable>
-                <Droppable droppableId="droppable2">
+                <Droppable droppableId="droppableMids" isDropDisabled={this.state.draggedPlayerPosition != 'MID'}>
                     {(provided, snapshot) => (
                         <div
                             ref={provided.innerRef}
                             style={getListStyle(snapshot.isDraggingOver)}>
-                            {this.state.selected.map((item, index) => (
+                            {this.state.mids.map((item, index) => (
                                 <Draggable
                                     key={item.id}
                                     draggableId={item.id}
@@ -173,7 +215,91 @@ class MyTeam extends Component {
                                                 snapshot.isDragging,
                                                 provided.draggableProps.style
                                             )}>
-                                            {item.content}
+                                            {item.content.name}
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+                <Droppable droppableId="droppableRucs" isDropDisabled={this.state.draggedPlayerPosition != 'RUC'}>
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}>
+                            {this.state.rucs.map((item, index) => (
+                                <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    index={index}>
+                                    {(provided, snapshot) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={getItemStyle(
+                                                snapshot.isDragging,
+                                                provided.draggableProps.style
+                                            )}>
+                                            {item.content.name}
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+                <Droppable droppableId="droppableFwds" isDropDisabled={this.state.draggedPlayerPosition != 'FWD'}>
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}>
+                            {this.state.fwds.map((item, index) => (
+                                <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    index={index}>
+                                    {(provided, snapshot) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={getItemStyle(
+                                                snapshot.isDragging,
+                                                provided.draggableProps.style
+                                            )}>
+                                            {item.content.name}
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+                <Droppable droppableId="droppableBench">
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}>
+                            {this.state.bench.map((item, index) => (
+                                <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    index={index}>
+                                    {(provided, snapshot) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={getItemStyle(
+                                                snapshot.isDragging,
+                                                provided.draggableProps.style
+                                            )}>
+                                            {item.content.name}
                                         </div>
                                     )}
                                 </Draggable>
