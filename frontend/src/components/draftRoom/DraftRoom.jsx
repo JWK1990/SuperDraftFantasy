df0bc1ae1f218973b30b6cd57b88bc4f46284d68
JWK1990 <jwkumm@gmail.com>
2020-03-03 22:09:13 +0100

    Change 2 to test Build and Release Pipeline.

--- frontend/src/components/draftRoom/DraftRoom.jsx
+++ frontend/src/components/draftRoom/DraftRoom.jsx
@@ -310,7 +310,7 @@ class DraftRoom extends React.Component {
         updatedBlock.onTheBlockCoach = onTheBlockCoach;
         this.setState({block: updatedBlock});
         console.log('Block Set: ', updatedBlock);
-        console.log('Change to test Production Build Pipeline.');
+        console.log('Change to test Production Build Pipeline 2.');
     }
 
     getPlayers = () => {
