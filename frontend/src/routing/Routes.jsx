2b7138c1239b191d615e59d98683c966667bc6d8
KUMM019 <joshuawade.kumm@arvato.com>
2020-11-16 11:02:10 +0100

    Tidied up imports in Routes file.

--- frontend/src/routing/Routes.jsx
+++ frontend/src/routing/Routes.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import AuthenticatedNavbar from "../components/navbar";
 import PrivateRoute from "./PrivateRoute";
 import DraftRoom from "../components/draftRoom";
