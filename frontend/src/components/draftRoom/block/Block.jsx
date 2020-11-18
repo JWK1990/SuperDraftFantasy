c9d6a462b9acdea125c398e0dee8e02347a212ac
KUMM019 <joshuawade.kumm@arvato.com>
2020-11-18 09:20:26 +0100

    Added BlockPlayer and CircularStatIcon Components to display OnTheBlock Player Stats.

--- frontend/src/components/draftRoom/block/Block.jsx
+++ frontend/src/components/draftRoom/block/Block.jsx
@@ -4,31 +4,9 @@ import CountdownClock from "./clock/CountdownClock";
 import Grid from "@material-ui/core/Grid";
 import {connect} from "react-redux";
 import {currentTeamSelector} from "../../../store/selectors/DraftSelectors";
+import BlockPlayer from "./player/BlockPlayer";
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    details: {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    content: {
-        flex: '1 0 auto',
-    },
-    cover: {
-        width: 151,
-    },
-    controls: {
-        display: "flex",
-        alignItems: "center",
-        paddingLeft: theme.spacing(1),
-        paddingBottom: theme.spacing(1),
-    },
-    playIcon: {
-        height: 38,
-        width: 38,
-    },
     firstRowGridContainer: {
         justify: "space-between",
         alignItems: "stretch",
@@ -279,8 +257,7 @@ class DraftRoomBlock extends React.Component {
                 container
                 className={classes.firstRowGridContainer}
             >
-                <Grid item xs={2}
-                >
+                <Grid item xs={2}>
                     <div className={classes.countdownClockDiv}>
                         {!!this.state.block.clockState ?
                             <CountdownClock
@@ -298,10 +275,13 @@ class DraftRoomBlock extends React.Component {
                         }
                     </div>
                 </Grid>
-                <Grid item xs={2}>
-                </Grid>
+
                 <Grid item xs={8}>
+                    <BlockPlayer
+                        player={this.state.block.player}
+                    />
                 </Grid>
+
             </Grid>
 
 
---
+++ frontend/src/components/draftRoom/block/player/CircularStatIcon.jsx
@@ -0,0 +1,101 @@
+import React, {useEffect} from 'react';
+import clsx from 'clsx';
+import {makeStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {green, orange, red, yellow} from '@material-ui/core/colors';
+import Fab from '@material-ui/core/Fab';
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingLeft: 40,
+    },
+    iconRoot: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    wrapper: {
+        margin: theme.spacing(1),
+        position: 'relative',
+    },
+    fabProgress: {
+        position: 'absolute',
+        top: -6,
+        left: -6,
+        zIndex: 1,
+        transform: "rotate(0deg)!important"
+    },
+    greenBar: {
+        color: green[500],
+    },
+    yellowBar: {
+        color: yellow[500],
+    },
+    orangeBar: {
+        color: orange[500],
+    },
+    redBar: {
+        color: red[500],
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: "bold",
+    }
+}));
+
+export default function CircularStatIcon(props) {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    // TODO: Could update this to represent how high in the rankings they are.
+    const statRanking = props.statValue/props.maxStatValue;
+    const barColour = clsx({
+        [classes.greenBar] : statRanking >= 0.85,
+        [classes.yellowBar] : (0.85 > statRanking) && (statRanking >= 0.7),
+        [classes.orangeBar] : (0.70 > statRanking) && (statRanking >= 0.5),
+        [classes.redBar] : 0.5 > statRanking,
+    })
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setValue(value => {
+                let updatedValue = value + 1;
+                if(updatedValue >= props.statValue) {
+                    clearInterval(interval)
+                }
+                return updatedValue
+            });
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [props.statValue]);
+
+    return (
+        <div className={classes.root}>
+            <Typography
+                align="center"
+                className={classes.title}
+            >
+                {props.statName}
+            </Typography>
+            <div className={classes.iconRoot}>
+                    <div className={classes.wrapper}>
+                        <Fab
+                            aria-label="save"
+                            color="primary"
+                        >
+                            {value}
+                        </Fab>
+                        <CircularProgress
+                            size={68}
+                            className={[classes.fabProgress, barColour].join(" ")}
+                            variant="static"
+                            value={value/props.maxStatValue * 100}
+                        />
+                    </div>
+                </div>
+        </div>
+    );
+}
