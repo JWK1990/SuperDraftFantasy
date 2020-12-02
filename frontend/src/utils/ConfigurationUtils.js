9009f40e9785113810d633718239e165c7d3e4ad
KUMM019 <joshuawade.kumm@arvato.com>
2020-12-02 12:19:10 +0100

    Added Dockerization.
    - The SuperDraftFantasy Application can now be run in Docker.
    - There are 2 Dockerfiles (1 for Frontend and 1 for Backend).
    - There is a Docker-Compose file, which builds the 2 Dockerfiles as well as the Postgresql DB.
    - The Frontend is run using nginx and the Backend uses Mvn Wrapper for installation.

--- frontend/src/utils/ConfigurationUtils.js
+++ frontend/src/utils/ConfigurationUtils.js
@@ -1,7 +1,9 @@
 class ConfigurationUtils {
 
+    // TODO: Could create an exported const and move variable to /src/constants.index.js.
+    // For example export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';.
     getBaseUrl() {
-        return process.env.NODE_ENV === 'production' ? "" : "http://localhost:8080";
+        return process.env.NODE_ENV === 'production' ? "http://localhost:8080" : "http://localhost:8080";
     }
 
     getWebsocketUrl() {
