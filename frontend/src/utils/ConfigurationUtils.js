7e26d9dfe151e03733f75fc1d4380c5a74afafdc
KUMM019 <joshuawade.kumm@arvato.com>
2020-12-07 00:02:44 +0100

    Trying approach 3.

--- frontend/src/utils/ConfigurationUtils.js
+++ frontend/src/utils/ConfigurationUtils.js
@@ -4,7 +4,7 @@ class ConfigurationUtils {
     // For example export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';.
     getBaseUrl() {
         console.log("Base URL: ", process.env.REACT_APP_API_BASE_URL);
-        return process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+        return process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_SERVER : "http://localhost:8080/api";
     }
 
     getWebsocketUrl() {
