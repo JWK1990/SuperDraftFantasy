602c269104c4cfaf8e6b3bfd7f8a1fd6b171f112
KUMM019 <joshuawade.kumm@arvato.com>
2020-12-06 19:44:41 +0100

    Working locally. Reverted to previous method.

--- frontend/src/utils/ConfigurationUtils.js
+++ frontend/src/utils/ConfigurationUtils.js
@@ -3,7 +3,8 @@ class ConfigurationUtils {
     // TODO: Could create an exported const and move variable to /src/constants.index.js.
     // For example export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';.
     getBaseUrl() {
-        return 'http://localhost:8080/api';
+        console.log("Base URL: ", process.env.REACT_APP_API_BASE_URL);
+        return process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
     }
 
     getWebsocketUrl() {
