0ead8bb5016b05b047b1767580ae435774487f7b
KUMM019 <joshuawade.kumm@arvato.com>
2020-12-08 19:37:38 +0100

    Added Develop docker-compose.

--- frontend/src/utils/ConfigurationUtils.js
+++ frontend/src/utils/ConfigurationUtils.js
@@ -1,7 +1,6 @@
 class ConfigurationUtils {
 
     getBaseUrl() {
-        console.log("Base URL: ", process.env.REACT_APP_API_BASE_URL);
         return process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_SERVER + "/api" : "http://localhost:8080/api";
     }
 
