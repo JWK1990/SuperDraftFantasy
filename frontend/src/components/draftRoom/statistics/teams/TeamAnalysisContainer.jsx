5cc11021bf567b9584e229e6480895d55d15c3bf
joshuawade.kumm <joshuawade.kumm@arvato-scs.com>
2021-02-26 22:41:21 +0100

    Added initial TeamStats Table.

--- frontend/src/components/draftRoom/statistics/teams/TeamAnalysisContainer.jsx
+++ frontend/src/components/draftRoom/statistics/teams/TeamAnalysisContainer.jsx
@@ -7,6 +7,7 @@ import {changeDraftStatisticsTeamIdAction} from "../../../../store/actions/Navig
 import {currentDraftStatisticsTeamIdSelector} from "../../../../store/selectors/NavigationSelectors";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
+import TeamStats from "./teamStats/TeamStats";
 
 const styles = {
     teamStatsContainer: {
@@ -17,6 +18,7 @@ const styles = {
         width: "100%",
     }
 }
+
 class TeamAnalysisContainer extends React.Component {
 
     componentWillMount() {
@@ -40,7 +42,7 @@ class TeamAnalysisContainer extends React.Component {
             <div className="teamAnalysis">
                 <Grid container spacing={0} direction="row" justify="space-between" alignItems="stretch">
                     <Grid item xs={12}>
-                        <div className="teamSelect centered-div">
+                        <div className="centered-div">
                             <TextField
                                 id="draft-team-analysis-select"
                                 select
@@ -56,10 +58,18 @@ class TeamAnalysisContainer extends React.Component {
                             </TextField>
                         </div>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
+                        <Grid container spacing={0} direction="row" justify="space-between" alignItems="stretch">
+                            <Grid item xs={12}>
+                                <TeamStats />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <p>Test 2</p>
+                            </Grid>
+                        </Grid>
                     </Grid>
                     {/* Change the below width to change the size of the TeamFieldView. */}
-                    <Grid item xs={4}>
+                    <Grid item xs={5}>
                         <div className={classes.teamFieldViewContainer}>
                             <TeamViewField />
                         </div>
---
+++ frontend/src/components/draftRoom/statistics/teams/teamStats/TeamStats.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import {
+    createMuiTheme,
+    MuiThemeProvider,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = {
+    teamStatsTable: {
+        //minWidth: 650,
+    },
+    rowHeader: {
+        borderRight: "1px solid black",
+        color: "rgba(0, 0, 0, 0.87)",
+        fontWeight: 500,
+    },
+    defHeader: {
+        color: "rgba(var(--def-color-primary), 0.8)",
+    },
+    defColumn: {
+        backgroundColor: "rgba(var(--def-color-secondary), 0.2)",
+    },
+    midHeader: {
+        color: "rgba(var(--mid-color-primary), 0.8)",
+    },
+    midColumn: {
+        backgroundColor: "rgba(var(--mid-color-secondary), 0.2)",
+    },
+    rucHeader: {
+        color: "rgba(var(--ruc-color-primary), 0.8)",
+    },
+    rucColumn: {
+        backgroundColor: "rgba(var(--ruc-color-secondary), 0.2)",
+    },
+    fwdHeader: {
+        color: "rgba(var(--fwd-color-primary), 0.8)",
+    },
+    fwdColumn: {
+        backgroundColor: "rgba(var(--fwd-color-secondary), 0.2)",
+    },
+    summaryColumn: {
+        color: "rgba(0, 0, 0, 0.87)",
+        fontWeight: 500,
+    }
+}
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiTableCell: {
+            root: {
+                textAlign: "center",
+            },
+            head: {
+                borderBottom: "1px solid black",
+                width: "40px",
+                textAlign: "center",
+            }
+        },
+    }
+});
+
+class TeamStats extends React.Component {
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <MuiThemeProvider theme={theme}>
+                <TableContainer component={Paper}>
+                    <Table className={classes.teamStatsTable} aria-label="Player Stat Bar">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className={classes.rowHeader}/>
+                                <TableCell className={[classes.defHeader, classes.defColumn].join(" ")}>DEF</TableCell>
+                                <TableCell className={[classes.midHeader, classes.midColumn].join(" ")}>MID</TableCell>
+                                <TableCell className={[classes.rucHeader, classes.rucColumn].join(" ")}>RUC</TableCell>
+                                <TableCell className={[classes.fwdHeader, classes.fwdColumn].join(" ")}>FWD</TableCell>
+                                <TableCell>ALL</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className={classes.rowHeader}>Count</TableCell>
+                                <TableCell className={classes.defColumn}>1</TableCell>
+                                <TableCell className={classes.midColumn}>2</TableCell>
+                                <TableCell className={classes.rucColumn}>8</TableCell>
+                                <TableCell className={classes.fwdColumn}>7</TableCell>
+                                <TableCell className={classes.summaryColumn}>5</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={classes.rowHeader}>SC</TableCell>
+                                <TableCell className={classes.defColumn}>26 (#1)</TableCell>
+                                <TableCell className={classes.midColumn}>50 (#3)</TableCell>
+                                <TableCell className={classes.rucColumn}>110 (#2)</TableCell>
+                                <TableCell className={classes.fwdColumn}>100 (#10)</TableCell>
+                                <TableCell className={classes.summaryColumn}>40 (#1)</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={classes.rowHeader}>Spend</TableCell>
+                                <TableCell className={classes.defColumn}>$70</TableCell>
+                                <TableCell className={classes.midColumn}>$10</TableCell>
+                                <TableCell className={classes.rucColumn}>$5</TableCell>
+                                <TableCell className={classes.fwdColumn}>$7</TableCell>
+                                <TableCell className={classes.summaryColumn}>$8</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </MuiThemeProvider>
+        )
+    }
+
+}
+
+export default withStyles(styles)(TeamStats);
