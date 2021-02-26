821ac150771bc225f12d41535956a4c7c908a9f6
joshuawade.kumm <joshuawade.kumm@arvato-scs.com>
2021-02-26 08:48:48 +0100

    Updated TeamAnalysisContainer to use Grid Layout.

--- frontend/src/components/draftRoom/statistics/teams/TeamAnalysisContainer.jsx
+++ frontend/src/components/draftRoom/statistics/teams/TeamAnalysisContainer.jsx
@@ -6,11 +6,15 @@ import TeamViewField from "./fieldView/TeamViewField";
 import {changeDraftStatisticsTeamIdAction} from "../../../../store/actions/NavigationActions";
 import {currentDraftStatisticsTeamIdSelector} from "../../../../store/selectors/NavigationSelectors";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
+    teamStatsContainer: {
+        width: "100%",
+    },
     // Use the below width to control the size of the TeamFieldView.
     teamFieldViewContainer: {
-        width: "40%",
+        width: "100%",
     }
 }
 class TeamAnalysisContainer extends React.Component {
@@ -31,27 +35,36 @@ class TeamAnalysisContainer extends React.Component {
         if(!this.props.selectedTeamId) {
             return null;
         }
+
         return (
             <div className="teamAnalysis">
-                <div className="teamSelect" style={{display:"none"}}>
-                    <TextField
-                        id="draft-team-analysis-select"
-                        select
-                        value={this.props.selectedTeamId}
-                        onChange={this.handleTeamChange}
-                        helperText="Select Team To Analyse."
-                    >
-                        {this.props.teamNameList.map((team) => (
-                            <MenuItem key={team.id} value={team.id}>
-                                {team.name}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                </div>
-                <div className={classes.teamFieldViewContainer}>
-                    <TeamViewField />
-                </div>
+                <Grid container spacing={0} direction="row" justify="space-between" alignItems="stretch">
+                    <Grid item xs={8}>
+                        <div className="teamSelect">
+                            <TextField
+                                id="draft-team-analysis-select"
+                                select
+                                value={this.props.selectedTeamId}
+                                onChange={this.handleTeamChange}
+                                helperText="Select Team To Analyse."
+                            >
+                                {this.props.teamNameList.map((team) => (
+                                    <MenuItem key={team.id} value={team.id}>
+                                        {team.name}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </div>
+                    </Grid>
+                    {/* Change the below width to change the size of the TeamFieldView. */}
+                    <Grid item xs={4}>
+                        <div className={classes.teamFieldViewContainer}>
+                            <TeamViewField />
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
+
         )
     }
 }
