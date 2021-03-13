144b38ada590aa16c16383946a3e7352091b5bd4
joshuawade.kumm <joshuawade.kumm@arvato-scs.com>
2021-03-13 16:12:45 +0100

    Added PlayerPictures.

--- frontend/src/components/draftRoom/block/details/purchaseReview/PurchaseReviewRatingCard.jsx
+++ frontend/src/components/draftRoom/block/details/purchaseReview/PurchaseReviewRatingCard.jsx
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import {makeStyles} from "@material-ui/core/styles";
-import PurchaseReviewSymbolFetcher from "./PurchaseReviewSymbolFetcher";
+import PurchaseReviewSymbolFetcher from "../../../../shared/imageFetchers/PurchaseReviewSymbolFetcher";
 
 const useStyles = makeStyles((theme) => ({
     root: {
