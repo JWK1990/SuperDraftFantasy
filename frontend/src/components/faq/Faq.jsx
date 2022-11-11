c76da451d2d308fea2af950ca7cba60999ef773f
joshuawade.kumm <joshuawade.kumm@arvato-scs.com>
2022-11-11 01:01:32 +0100

    Comment out iframe on Home page.
    - This will eventually contain the introductory video, however, this needs to be removed to film the introductory video.
    - Remove unnecessary imports in Faq.

--- frontend/src/components/faq/Faq.jsx
+++ frontend/src/components/faq/Faq.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import CardContent from "@material-ui/core/CardContent";
-import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import {makeStyles, Paper} from "@material-ui/core";
 
